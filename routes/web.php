3f2b4e430fd1170e30a917e4fa26612caf989bd1
Usman Arif <usmanarif.9219@gmail.com>
2024-09-09 16:33:10 +0500

    fixing profile issues

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,33 @@
 <?php
 
 use App\Http\Controllers\VCardController;
+use App\Models\Profile;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
 Route::view('/', 'welcome');
+
+Route::get('/set/default/profile', function () {
+    $users = User::select('id')
+        ->orderBy('id')
+        ->get();
+    foreach ($users as $user) {
+        Profile::where('user_id', $user->id)
+            ->update([
+                'is_default' => 0
+            ]);
+
+        $first_profile = Profile::where('user_id', $user->id)->first();
+        if ($first_profile) {
+            Profile::where('id', $first_profile->id)->update([
+                'is_default' => 1
+            ]);
+        }
+    }
+
+    dd("done");
+});
 
 Route::get('/privacy-and-policy', function () {
     return view('landingpage.privacy-policy');
@@ -53,6 +75,7 @@ Route::view('/{username}', 'profile');
 Route::get('/account-deletion/policy', function () {
     return view('pages.account-deletion-policy');
 });
+
 
 
 Route::get('save_contact/{id}', [VCardController::class, 'saveContact'])->name('save.contact');
