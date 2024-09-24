8fd051fa9b9ddda2ce86a9d76f80c9aa6bee788f
Abdul Haseeb <haseeb7063952@gmail.com>
2024-09-24 18:55:17 +0500

    Dashboard Updated

--- resources/views/layouts/admin/partials/sidebar.blade.php
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -1,5 +1,18 @@
-<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme position-relative">
     <livewire:admin.sidebar />
+
+    <div class="position-absolute bottom-0 m-3">
+        <button class="btn rounded-3 d-flex align-items-center gap-2 w-100" style="background: #000">
+            <div>
+                <img src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?t=st=1727185449~exp=1727189049~hmac=174cae49c8da90bd3f8d1308bc89418301d16fb1276a0c399cc7b20f7f47af4b&w=740"
+                    height="25px" width="25px" class="img-fluid rounded-circle" alt="">
+            </div>
+            <div>
+                <h6 class="text-white m-0 text-start" style="font-size: 12px">Haseeb</h6>
+                <p class="text-white m-0 text-truncate" style="font-size: 10px">haseeb@gmail.com</p>
+            </div>
+        </button>
+    </div>
 </aside>
 
 
