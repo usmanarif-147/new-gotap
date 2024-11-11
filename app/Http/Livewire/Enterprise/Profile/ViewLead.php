1b6aeb483472427471fc692f27753a78c5458885
mudasarali123 <95418611+mudasarali123@users.noreply.github.com>
2024-11-11 20:09:34 +0500

    sidebar change

--- app/Http/Livewire/Enterprise/Profile/ViewLead.php
+++ app/Http/Livewire/Enterprise/Profile/ViewLead.php
@@ -23,20 +23,17 @@ class ViewLead extends Component
             'leads.viewer_id',
             'leads.created_at',
             DB::raw('COALESCE(viewerProfile.username, "No Viewer") as viewer_username'),
-            DB::raw('COALESCE(viewerProfile.photo, "photo") as viewer_photo'),
+            DB::raw('COALESCE(viewerProfile.photo, "photo") as viewer_photo')
         )
             // Join profiles on viewer_id
             ->leftJoin('profiles as viewerProfile', 'leads.viewer_id', '=', 'viewerProfile.id')
-            ->where(function ($query) {
-                // If viewer_id is null, just return the data; else, return profile data
-                $query->whereNull('leads.viewer_id');
-            })
             ->where('leads.id', $id)
-            ->orderBy('leads.created_at', 'desc')->first();
-
+            ->orderBy('leads.created_at', 'desc')
+            ->first();
 
         return $lead;
     }
+
 
     public function render()
     {
